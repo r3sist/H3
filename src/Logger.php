3a02efca18e82ba3b124d9a06e8ffc63addf6dbf
resist <vankos@resist.hu>
2019-12-22 16:58:55 +0100

    Fix query in create()

--- src/Logger.php
+++ src/Logger.php
@@ -30,7 +30,7 @@ class Logger
     public function create(string $group, string $title = '', string $body = ''): void
     {
         $query = 'INSERT INTO'.self::TABLE.' (uname, lgroup, ltitle, lbody, lts, lip) VALUES (:uname, :lgroup, :ltitle, :lbody, :lts, :lip)';
-        $this->db->exec('$query', [
+        $this->db->exec($query, [
             ':uname' => $this->f3->uname,
             ':lgroup' => $this->f3->clean($group),
             ':ltitle' => $this->f3->clean($title),
