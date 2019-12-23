f8b8488fca330608c837313f3b87547886dab9a8
resist <vankos@resist.hu>
2019-12-23 13:45:02 +0100

    Add H3 Shorten tests

--- tests/H3ShortenTest.php
+++ tests/H3ShortenTest.php
@@ -7,3 +7,19 @@ use Tester\Assert;
 Tester\Environment::setup();
 
 Assert::true(is_callable("H3::shorten"));
+
+Assert::true(H3::shorten('Lorem ipsum', 2, '') === 'Lo');
+Assert::true(H3::shorten('Lorem ipsum', 2) === 'Lo&hellip;');
+Assert::true(H3::shorten('Lorem ipsum', 100) === 'Lorem ipsum');
+
+Assert::exception(function () {
+    H3::shorten(10, 2);
+}, TypeError::class);
+
+Assert::exception(function () {
+    H3::shorten(new H3(), 2);
+}, TypeError::class);
+
+Assert::exception(function () {
+    H3::shorten('Lorem ipsum', 'x');
+}, TypeError::class);
