f0217d1bd3059a3338d482f76e0fd60fff857cec
r3sist <r3sist@users.noreply.github.com>
2020-03-08 20:16:55 +0100

    Update Logger, accepts array as body

--- src/Logger.php
+++ src/Logger.php
@@ -1,7 +1,4 @@
-<?php
-// L3 - Simple and dirty logging platform for Fatfree Framework powered apps
-// Log writer
-// (c) resist | https://github.com/r3sist/l3
+<?php declare(strict_types=1);
 
 namespace resist\H3;
 
@@ -12,9 +9,9 @@ use \DB\SQL\Mapper;
 class Logger
 {
     const TABLE = 'log';
+
     public Base $f3;
     private SQL $db;
-    public Mapper $map;
 
     public function __construct(Base $f3, SQL $db)
     {
@@ -22,16 +19,24 @@ class Logger
         $this->db = $db;
     }
 
-    public function registerMap()
+    public function getMap()
     {
-        $this->map = new Mapper($this->db, self::TABLE);
+        return new Mapper($this->db, self::TABLE);
     }
 
-    public function create(string $group, string $title = '', string $body = ''): void
+    /** @param string|array $body */
+    public function create(string $group, string $title = '', $body = ''): void
     {
+        if (is_array($body)) {
+            try {
+                $body = json_encode($body, JSON_THROW_ON_ERROR, 512);
+            } catch (\JsonException $e) {
+                $body = 'ORIGINAL LOG MESSAGE LOST! '.$e->getMessage();
+            }
+        }
         $query = 'INSERT INTO '.self::TABLE.' (uname, lgroup, ltitle, lbody, lts, lip) VALUES (:uname, :lgroup, :ltitle, :lbody, :lts, :lip)';
         $this->db->exec($query, [
-            ':uname' => ($this->f3->get('uname')?$this->f3->get('uname'):'__GUEST__'),
+            ':uname' => ($this->f3->exists('uname')?$this->f3->get('uname'):''),
             ':lgroup' => $this->f3->clean($group),
             ':ltitle' => $this->f3->clean($title),
             ':lbody' => $body,
