119563ee18888784750c8a4686c8dc6cf258a9b6
resist <vankos@resist.hu>
2020-08-13 12:19:10 +0200

    Md: clean code

--- src/Md.php
+++ src/Md.php
@@ -1,19 +1,21 @@
-<?php
-// H3 - Helpers for Fatfree Framework
-// Markdown helpers
-// (c) resist | https://github.com/r3sist/h3
+<?php declare(strict_types=1);
+/**
+ * H3/Logger - Markdown helpers.
+ * (c) 2020 resist | https://resist.hu | https://github.com/r3sist/h3
+ */
 
 namespace resist\H3;
 
-use \Base;
-use \Markdown;
+use Base;
+use Markdown;
 
 class Md
 {
-    public Base $f3;
+    private Base $f3;
     private Markdown $md;
-    const ONELINETAGS = 'i,em,b,strong,a,code';
-    const MULTILINETAGS = 'i,em,b,strong,a,code,ul,li,p,ol,h1,h2,h3,h4,h5,h6,hr,img';
+
+    private const ONE_LINE_TAGS = 'i,em,b,strong,a,code';
+    private const MULTI_LINE_TAGS = 'i,em,b,strong,a,code,ul,li,p,ol,h1,h2,h3,h4,h5,h6,hr,img';
 
     public function __construct(Base $f3, Markdown $md)
     {
@@ -24,13 +26,15 @@ class Md
     public function makeOneLine(string $string): string
     {
         $md = $this->md->convert($this->f3->clean($string));
-        return $this->f3->clean($md, self::ONELINETAGS);
+
+        return $this->f3->clean($md, self::ONE_LINE_TAGS);
     }
 
     public function makeWithoutHtml(string $string): string
     {
         $md = $this->md->convert($this->f3->clean($string, 'br'));
-        return $this->f3->clean($md, self::MULTILINETAGS);
+
+        return $this->f3->clean($md, self::MULTI_LINE_TAGS);
     }
 
     public function makeMd(string $string): string
